59f4acbd66ccae9f32280408fee5388534fd084e
Alphabetus <contact@alphawebmaker.com>
2021-02-06 22:57:12 +0000

    move backend modules into another directory

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
-const ToDo = require('./src/engine/ToDo.js').ToDo;
+const ToDo = require('./backend/ToDo.js').ToDo;
 const toDo = new ToDo;
 
 
